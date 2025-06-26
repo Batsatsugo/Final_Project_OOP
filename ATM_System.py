446ead9e6c944b55bee77850ad47735d86980334
Batsatsugo <corajianchristian@gmail.com>
2025-06-26 15:39:21 +0800

    added instance method

--- ATM_System.py
+++ ATM_System.py
@@ -167,3 +167,5 @@ class ATMApp:
 
         Button(win, text="Confirm", command=confirm).pack(pady=5)
         Button(win, text="Cancel", command=win.destroy).pack(pady=5)
+
+    def update_balance(self):
